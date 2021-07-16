2a7f99e7b50bc7883549a2598fd354e3fb409851
keerthu1604 <87022313+keerthu1604@users.noreply.github.com>
2021-07-16 14:29:18 +0530

    Add files via upload

---
+++ positive num list2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 14:27:21 2021
+
+@author: keekee
+"""
+
+list2 = [12, 14, -95, 3]
+  
+for num in list2:
+      
+    if num >= 0:
+       print(num, end = " ")
